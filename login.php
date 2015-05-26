67b6246c13a7f1f1f500ce3e9304048d588c7505
Jose Contreras <jocontre@joselcontreras.com>
2015-05-26 00:05:10 -0500

    Added session and updated credentials

--- login.php
+++ login.php
@@ -1,32 +1,43 @@
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
 <?php 
-if (isset($_POST['name']) && isset($_POST['password'])) {
+  session_start();
+  
+  if (isset($_POST['name']) && isset($_POST['password'])) {
   $db = mysqli_connect('localhost', 'root', '', 'php');
   $sql = sprintf("SELECT * FROM users WHERE name='%s'",
     mysqli_real_escape_string($db, $_POST['name']);
   );
   $result = mysqli_query($db, $sql);
   $row = mysqli_fetch_assoc($result);
+  
   if($row) {
     $hash = $row['password'];
     $isAdmin = $row['isAdmin'];
     
     if(password_verify($_POST['password'], $hash)) {
-      echo 'Login successful';
+      $message = 'Login successful';
+      
+      $_SESSION['user'] = $row['name'];
+      $_SESSION['isAdmin'] = $isAdmin;
     }
     else {
-      echo 'login failed';
+      $message = 'login failed';
     }
+    
   }
   else {
-    echo 'Login failed';
+    $message = 'Login failed';
   }
   mysqli_close($db);
 }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php 
+  readfile('navigation.html');
+  echo "<p>$message</p>"
 ?>
 <form method="post" action="">
   Username<input type="text" name="name"><br>
